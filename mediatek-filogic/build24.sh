cd37ff5e58115f71fec919e3e68b69c968e1cbd0
Fang <82165052+fangenhui520@users.noreply.github.com>
2026-08-18 21:44:24 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -66,6 +66,7 @@ PACKAGES="$PACKAGES kmod-fs-exfat"
 PACKAGES="$PACKAGES kmod-fs-ext4"
 PACKAGES="$PACKAGES kmod-fs-vfat"
 PACKAGES="$PACKAGES ddns-scripts-cloudflare"
+PACKAGES="$PACKAGES xray-core"
 
 
 # 第三方软件包 合并
